e95c489b1b00d1a25bcd745b2fa3d559122778ae
Sam Guard <sam.guard@outlook.com>
2022-02-22 15:36:03 +0000

    change from plotting pixels to objects in vis

--- data_visualiser/visuals.py
+++ data_visualiser/visuals.py
@@ -15,10 +15,8 @@ csvData = open("../output/agent_pos_data.csv", mode='r', newline="")
 
 reader = csv.reader(csvData, delimiter=',')
 
-isHeader = True
-
-scale = WIDTH / 100
-offset = 100
+scale = WIDTH / 1000
+offset = WIDTH / 2
 
 def applyTransforms(x, y):
   x *= scale
@@ -27,18 +25,38 @@ def applyTransforms(x, y):
   y += offset
   return [x, y]
 
-for row in reader:
-    if(isHeader):
-        isHeader = False
+
+startTime = 0
+
+
+header = next(reader)
+agents = []
+
+
+for agent in header:
+    if(agent == "tick"):
         continue
-    rect.undraw()
-    rect.draw(win)
+    agents.append(Oval(Point(10,10), Point(5, 5)))
+    agents[-1].setFill("black")
+    agents[-1].draw(win)
+
+
+for row in reader:
+    startTime = time.time_ns()
     for i in range(1, len(row), 2):
         x = float(row[i])
         y = float(row[i+1])
 
         [x, y] = applyTransforms(x, y)
-        win.plot(x, y)
+        
+        cPoint = agents[i-1].getP2()
+        cPos = [cPoint.getX(), cPoint.getY()]
+        agents[i-1].move(x - cPos[0], y - cPos[1])
+
+        #win.plot(x, y)
     update()
+    timeDiff = 50000000 - (time.time_ns() - startTime)
+    if(timeDiff > 0):
+        time.sleep(timeDiff / 1000000000)
     #time.sleep()
 
